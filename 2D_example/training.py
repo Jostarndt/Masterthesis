a780d879296845f230d231f3b4196f016bab0d50
Jost Arndt <jost.arndt@hhi.fraunhofer.de>
2021-09-02 12:46:48 +0200

    Changes done before vacation. I am not sure if this is old or new - the running version is anyways in a different folder: /to_matthias/

--- 2D_example/training.py
+++ 2D_example/training.py
@@ -1,24 +1,120 @@
 import numpy as np
 import dgl
-import model
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import torch.linalg as la
-from copy import deepcopy
-from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
+import itertools
 import pdb
+from torch.utils.data import TensorDataset, ConcatDataset
 
 
 
-batchsize = 128
+batchsize = 4096
+class polynomial_linear_actor(nn.Module):
+    def __init__(self):
+        super(polynomial_linear_actor, self).__init__()
+    def forward(self, x):
+        square = torch.square(x) #(x,y) -> (x^2, y^2)
+        prod = torch.prod(x, 3).unsqueeze(3) #(x, y) -> x*y
+        output = torch.cat((x, square, prod), 3)
+        return output
+
+class polynomial_linear_critic(nn.Module):
+    def __init__(self):
+        print('correct function')
+        super(polynomial_linear_critic, self).__init__()
+    def forward(self, x):
+        square = torch.square(x)
+        prod = torch.prod(x,2).unsqueeze(2)
+        const = torch.ones_like(prod)
+
+        #output = torch.cat((square, prod, const), 2)#adding a const leads to missing invertability
+        output = torch.cat((square, prod), 2)
+        return output
+
+
+class Dataset():
+    def __init__(self):
+        self.support_points =20  #amount of euler steps / steps in the integral
+        self.amount_x = 2 #amount of points of x on which V is getting trained. Note: trajectory has actual lengh of amount_x * support_points
+        self.stepsize = 0.01 #stepsize for the euler steps, stepsize = distance of x from above / support points => distance of 0.3 
+        #NOTE!! if you change this you have to change 'approx_costs' in train.py TWO TIMES!
+        self.dataset = []
+        self.datasets = []
+        self.pde = dgl()
+
+    def create_double(self, starting_point = None, control_value = None):
+        '''creation of starting point'''
+        trajectory, controls = self.pde.euler_step(stepsize = self.stepsize, total_steps = self.support_points, last_point = starting_point, control= control_value)
+        return trajectory, controls
+
+    def create_dataset(self, control_value = None, starting_point = None):
+        x_output = torch.empty(self.amount_x, self.support_points +1 , starting_point.size()[0], starting_point.size()[1])
+        u_output = torch.empty(self.amount_x, self.support_points +1 , starting_point.size()[0], 1)
+ 
+        x, u = self.create_double(starting_point = starting_point, control_value = control_value)
+        x_output[0] = x
+        u_output[0] = u
+        for i in range(self.amount_x-1):
+            x, u = self.create_double(starting_point = x_output[i][-1], control_value = control_value)
+            x_output[i+1] = x
+            u_output[i+1] = u
+        dataset = TensorDataset(x_output, u_output)
+        return dataset
+
+    def create_dataset_different_control_and_starts(self, amount_startpoints):
+        controls = [[-i/10,-j/10] for i,j in zip(range(1, 5), range(1, 5))]
+        #controls = [[-i/10,-j/10] for i,j in itertools.product(range(1, 5), range(1, 5))]
+        print(controls)
+        controls = torch.unsqueeze(torch.tensor(controls, dtype= torch.float), 1)#TODO this in unncescessary - the controls & staring points are vectors anyways?
+        #controls = [-i/2 for i in range(7)]
+        #controls = torch.unsqueeze(torch.unsqueeze(torch.tensor(controls, dtype= torch.float), 1), 1)#TODO this in unncescessary - the controls & staring points are vectors anyways?
+        starting_points =np.random.rand(amount_startpoints,2)
+        starting_points =torch.unsqueeze(torch.tensor(starting_points, dtype= torch.float), 1)*0.1 #multiplication to adapt to starting point given in problem formulation
+
+        print(starting_points)
+
+
+
+        datasets=[]
+        for i in itertools.product(controls, starting_points):
+            datasets.append(self.create_dataset(control_value=i[0], starting_point=i[1]))
+        datasets= ConcatDataset(datasets)
+        return datasets
+
+
+
+class dgl:
+    def __init__(self, x_0 = None):
+        self.x_0 = torch.tensor([[1]], dtype= torch.float) if not x_0 else x_0
+
+    def rhs(self, x, u):
+        first_part =torch.unsqueeze(torch.unsqueeze( -x[0][0] + x[0][1], 0), 1)
+        second_part = -0.5*(x[0][0] + x[0][1]) + 0.5* x[0][0]**2*x[0][1] + x[0][0]*u
+        output = torch.stack((first_part, second_part))
+        return torch.squeeze(output ,1)#torch.matmul(self.A,x) +torch.matmul(self.B,u)
+
+    def euler_step(self, stepsize = 0.1,total_steps = 1, last_point=None, control = None):
+        output_traj = torch.unsqueeze(last_point, 1)
+        con_value = torch.matmul(control, last_point.transpose(0,1))
+        output_control = torch.unsqueeze(con_value, 1)#maybe do not unsqueeze it?!
+        for i in range(total_steps):
+            last_point = last_point + stepsize * self.rhs(last_point, con_value).transpose(-1,0)
+            output_traj = torch.cat((output_traj,torch.unsqueeze(last_point, 1)))
+            con_value = torch.matmul(control, last_point.transpose(0,1))
+            output_control = torch.cat((output_control,torch.unsqueeze(con_value, 1)))
+
+        return output_traj, output_control
+
+
+
 
 
 
 class error():
     def __init__(self):
-        self.costs = dgl.cost_functional()
         self.Q = torch.tensor([[1, 0], [0, 1]],dtype=torch.float)
         self.R = torch.tensor([[1]], dtype = torch.float)
 
@@ -37,7 +133,7 @@ class error():
         control_approx = torch.matmul(control_monomials, theta_u)
         rho_psi = torch.mul(torch.matmul(control_approx, self.R), control_approx).mean(1)
         
-        rho_u_psi= torch.matmul(control_approx.unsqueeze(3) - control , control_monomials).mean(1)#torch.matmul(control*control_monomials, theta_u)
+        rho_u_psi= 2* torch.matmul(control_approx.unsqueeze(3) - control , control_monomials).mean(1)#torch.matmul(control*control_monomials, theta_u)
         
         pi = rho_q + rho_psi.squeeze()
 
@@ -49,88 +145,38 @@ class error():
         #the equation system is now:     z*torch.cat((theta_v, theta_u)) = pi
         # to achieve this, calculate: 
         
-        #pdb.set_trace()
         #theta = la.lstsq(z, pi).solution #need to update pytorch.
 
         #or do it by hand:
         theta =torch.matmul(torch.matmul( torch.inverse(torch.matmul(z.transpose(0,1), z)), z.transpose(0,1)),pi) #not invertable?!
         #theta = (z * z )^(-1) *z * pi
-        theta_v, theta_u = torch.split(theta, [3,4], dim = 0)#TODO implement torch.size()[] instead of hard coding
+        theta_v, theta_u = torch.split(theta, [3,5], dim = 0)#TODO implement torch.size()[] instead of hard coding
         #return residual, theta_v, theta_u
         residual = torch.abs(torch.matmul(z, theta) - pi).sum()
         return residual, theta_v, theta_u
     
-def present_results(value_function, new_control, stepname= "after_training"):
-    int_const = value_function(torch.tensor([[0,0]], dtype= torch.float)).detach()
-    print(int_const, ' - this is the integration constant')
-    op_val_img = np.zeros((3,100,100))
-    op_con_img = np.zeros((3,100,100))
-    con_img = np.zeros((3,100,100))
-    val_img = np.zeros((3,100,100))
-    for x_1 in range(100):
-        for x_2 in range(100):
-            #print("tuple: ", x_1/1000, x_2/1000)
-            op_con_img[0][x_1][x_2] = - x_1/1000 * x_2/1000 
-            op_val_img[0][x_1][x_2] = 0.5*(x_1/1000) **2 + (x_2/1000 )**2
-            val_img[0][x_1][x_2] = value_function(torch.tensor([[x_1/1000 , x_2/1000 ]], dtype = torch.float)).detach() - int_const
-            #print(x_1/1000, x_2/1000)
-            con_img[0][x_1][x_2] = new_control(torch.tensor([[x_1/1000 , x_2/1000]], dtype = torch.float)).detach()
-    
-    #rescaling the images:
-    val_max = max(np.amax(val_img), np.amax(op_val_img))
-    val_min = min(np.amin(val_img), np.amin(op_val_img))
-    con_max = max(np.amax(con_img), np.amax(op_con_img))
-    con_min = min(np.amin(con_img), np.amin(op_con_img))
-    
-    print(con_img[0], 'con img')
-    print(op_con_img[0], 'op con img')
-    print('-----------------------')
-
-    val_img = (val_img-val_min)/(val_max - val_min)
-    op_val_img = (op_val_img-val_min)/(val_max-val_min)
-
-    con_img = (con_img-con_min)/(con_max - con_min)
-    op_con_img = (op_con_img-con_min)/(con_max-con_min)
-    
-    Writer.add_image('optimal value/'+stepname, op_val_img , 0)
-    Writer.add_image('optimal control function/'+stepname, op_con_img , 0)
-    Writer.add_image('value_function/'+stepname, val_img , 0)
-    Writer.add_image('control_function/'+stepname, con_img , 0)
-    Writer.close()
-    print(con_img[0], 'con img')
-    print(op_con_img[0], 'op con img')
-    
-    print('-----------------------')
-    print(val_img[0], 'val img')
-    print(op_val_img[0], 'op val img')
-
 
 if __name__ == '__main__':
-    Writer = SummaryWriter()
     error = error()
-    dataset = dgl.Dataset()
-    trainset = dataset.create_dataset_different_control_and_starts(amount_startpoints=100)
+    dataset = Dataset()
+    trainset = dataset.create_dataset_different_control_and_starts(amount_startpoints=500)
     train_loader = DataLoader(dataset = trainset, batch_size = batchsize, shuffle =True)
 
     
     testset = dataset.create_dataset_different_control_and_starts(amount_startpoints=10)
     test_loader = DataLoader(dataset = testset, batch_size = 2, shuffle =True)#2 to bring this into batch format
-    
-    dataset_stretch_factor =  len(train_loader)/len(test_loader)
 
 
     print("##################################")
     
-    control_function = model.polynomial_linear_actor()
-    value_function = model.polynomial_linear_critic()
-    pdb.set_trace()
-    theta_u = torch.ones(4)
+    control_function = polynomial_linear_actor()
+    value_function = polynomial_linear_critic()
+    theta_u = torch.ones(5)
     theta_v = torch.ones(3)
     
-    theta_u =torch.tensor([0, 0, -1, 0], dtype = torch.float)
-    theta_v =torch.tensor([0.5, 1, 0], dtype = torch.float)
+    #theta_u =torch.tensor([0, 0,0,0, -1], dtype = torch.float)
+    #theta_v =torch.tensor([0.5, 1, 0], dtype = torch.float)
 
-    costs = dgl.cost_functional()
 
 
     #present results!
@@ -143,20 +189,3 @@ if __name__ == '__main__':
 
             residual, theta_v, theta_u = error.both_iterations_direct_solution(trajectory= x, control=u, old_control = control_function, value_function = value_function , theta_u= theta_u, theta_v= theta_v)
             print(residual, theta_v, theta_u)
-
-        '''TESTING'''
-        print('Test')
-        for j, (x, u) in enumerate(test_loader):
-            pass
-            #residual, _ , _ = error.both_iterations_direct_solution(trajectory= x, control=u, old_control = control_function, value_function = value_function , theta_u= theta_u, theta_v= theta_v)
-            #print(residual, theta_v, theta_u)
-    
-    #Presenting results
-    present_results(value_function, new_control)
-    print('parameters of value function', value_function.parameters())
-    for name, param in value_function.named_parameters():
-        print(name, param.data)
-        
-    print('done')
-
-
