fb22310edb2339f13001788fcbbaae86b92431db
Jost Arndt <jost.arndt@hhi.fraunhofer.de>
2021-11-26 19:33:35 +0100

    adaption to ARE example in thesis

--- ARE/2d_are.py
+++ ARE/2d_are.py
@@ -7,9 +7,9 @@ print('start')
 
 
 A = np.array([[-1, 1],[-0.5,-0.5]])
-B = np.array([[0, 0],[0,0]])
+B = np.array([[3, 2],[1,0]])
 Q = np.array([[1, 0],[0,1]])
-R = np.array([[1, 0],[0,1]])
+R = np.array([[2, 0],[0,2]])
 
 
 P = linalg.solve_continuous_are(A,B,Q,R)
@@ -19,7 +19,7 @@ print(P)
 print('Testing:')
 
 
-test = np.matmul(A.transpose(), P)+ np.matmul(P, A) + Q
+test = np.matmul(A.transpose(), P)+ np.matmul(P, A) + Q - np.matmul(P, np.matmul(B, np.matmul( np.linalg.inv(R), np.matmul(B.transpose(), P))))
 
 
 print(test)
@@ -29,4 +29,7 @@ print('done')
 
 
 
+#print('BR^ -1 B^t = ', np.matmul(B, np.matmul( np.linalg.inv(R), B.transpose())))
+#print('PBR^ -1 B^t P = ', np.matmul(P, np.matmul(B, np.matmul( np.linalg.inv(R), np.matmul(B.transpose(), P)))))
+print('optimal control: R^ -1 B^t P = ', np.matmul(np.linalg.inv(R), np.matmul(B.transpose(), P)))
 
